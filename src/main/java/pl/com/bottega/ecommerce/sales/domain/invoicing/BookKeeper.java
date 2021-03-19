9ea66565ba5ef52284a0c2ca5b24ce583f3acbd1
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-03-19 17:51:23 +0100

    Created "TaxInformation" class that enables "TaxCalculator" to store information about specific taxes in a HashMap object. Fixed formatting to Eclipse standards.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -26,25 +26,17 @@ public class BookKeeper {
 
         for (RequestItem item : request.getItems()) {
             Money net = item.getTotalCost();
+            TaxCalculator tc = new TaxCalculator();
             BigDecimal ratio = null;
             String desc = null;
 
-            switch (item.getProductData().getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
+            TaxInformation ti = tc.SearchTaxInformationByType(item.getProductData().getType());
 
-                default:
-                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
+            if (ti == null)
+                throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
+            else {
+                ratio = ti.getRatio();
+                desc = ti.getDesc();
             }
 
             Money taxValue = net.multiplyBy(ratio);
